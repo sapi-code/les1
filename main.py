25513df786ae1d4c5b9ac1f0768d102f5f666fd6
sapi-code <dmitriy-pinisov@mail.ru>
2024-06-25 18:09:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+a='Hello, my friend!'
+print(len(a))
+first = 5
+second = 2
+summa = first + second
+diff = first - second
+print(summa, diff)
+third = 20
+mean = (first + second + third) / 3
+print(mean)
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string, ", ", first_string)
+a = 5
+b = 4
+c = 3
+f = (a * b) + (a * c)
+print((f ** 3) / 2)
+
+
+
