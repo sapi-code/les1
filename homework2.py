5ed8f255fddf735f22135779de3c116da6b855b8
sapi-code <dmitriy-pinisov@mail.ru>
2024-06-25 21:03:04 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+first_name = 'Dmitriy'
+how_much_homework = 12
+hours_spent = 1.5
+NameCours = 'Python'
+TaskTime = (hours_spent / how_much_homework)
+print('Курс:', NameCours, ',', 'всего задач:', how_much_homework, ',', 'затрачено часов:', hours_spent, ',', 'среднее время выполнения',TaskTime,'.')
+
